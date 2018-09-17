63a10b3dd9a2d3a7d9db15cc486d0a72f2693ebc
Anna Fredriksson <leliloro@gmail.com>
2018-09-17 18:08:58 +0200

    Login signup done

--- routes/auth.js
+++ routes/auth.js
@@ -2,29 +2,95 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const User = require('../models/user');
 
 /* GET auth/login page. */
 router.get('/login', (req, res, next) => {
-  res.render('login');
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  const messages = req.flash('login-error');
+  const data = {
+    message: messages[0]
+  };
+  res.render('login', data);
 });
 
 router.post('/login', (req, res, next) => {
-  // ...
-  console.log(req.body);
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    req.flash('login-error', 'Username and password are required');
+    return res.redirect('/auth/login');
+  }
+  User.findOne({ username })
+    .then(result => {
+      if (!result) {
+        req.flash('login-error', 'User doesnt exists');
+        return res.redirect('/auth/login');
+      }
+      if (!bcrypt.compareSync(password, result.password)) {
+        req.flash('login-error', 'User doesnt exists');
+        return res.redirect('/auth/login');
+      }
+      req.session.currentUser = result;
+      res.redirect('/');
+    })
+    .catch(next);
 });
 
 /* GET auth/signup page. */
 router.get('/signup', (req, res, next) => {
-  res.render('signup');
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  const messages = req.flash('signup-error');
+  const data = {
+    message: messages[0]
+  };
+  res.render('signup', data);
 });
 
 router.post('/signup', (req, res, next) => {
-  // ...
-  console.log(req.body);
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    req.flash('signup-error', 'Username and password are required');
+    return res.redirect('/auth/signup');
+  }
+  User.findOne({ username })
+    .then(result => {
+      if (result) {
+        req.flash('signup-error', 'Username already exists');
+        return res.redirect('/auth/signup');
+      }
+
+      // encrypt password
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      const user = new User({ username, password: hashedPassword });
+      return user.save()
+        .then(() => {
+          req.session.currentUser = user;
+          res.redirect('/');
+        });
+    })
+    .catch(next);
 });
 
 router.post('/logout', (req, res, next) => {
-  // ...
+  delete req.session.currentUser;
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Users' });
+  res.render('index');
+});
+router.get('/private-page', (req, res, next) => {
+  if (!req.session.currentUser) {
+    return res.redirect('/');
+  }
+  res.render('private');
 });
 
 module.exports = router;
